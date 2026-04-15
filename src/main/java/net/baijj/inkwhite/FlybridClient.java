7eadf703ae611892545651ee06278ea9fc31a429
bcieal321 <2168804617@qq.com>
2026-04-15 20:11:27 +0800

    ver improtant change

---
+++ src/main/java/net/baijj/inkwhite/FlybridClient.java
@@ -0,0 +1,10 @@
+package net.baijj.inkwhite;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlybridClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
