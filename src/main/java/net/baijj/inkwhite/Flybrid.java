dcfea91e6bb887880c166368f1f9a4436f638517
bcieal321 <2168804617@qq.com>
2026-04-19 21:16:39 +0800

    new change

--- src/main/java/net/baijj/inkwhite/Flybrid.java
+++ src/main/java/net/baijj/inkwhite/Flybrid.java
@@ -1,5 +1,7 @@
 package net.baijj.inkwhite;
 
+import net.baijj.inkwhite.item.ModItemGroups;
+import net.baijj.inkwhite.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -8,17 +10,11 @@ import org.slf4j.LoggerFactory;
 public class Flybrid implements ModInitializer {
 	public static final String MOD_ID = "ink-white";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/baijj/inkwhite/item/ModItemGroups.java
@@ -0,0 +1,24 @@
+package net.baijj.inkwhite.item;
+
+import net.baijj.inkwhite.Flybrid;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(Flybrid.MOD_ID, "ruby"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby"))
+                    .icon(() ->new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.RUBY);
+                        entries.add(ModItems.RAW_RUBY);
+                    }).build());
+
+    public static void registerItemGroups(){
+        Flybrid.LOGGER.info("Registing Item Groups for" + Flybrid.MOD_ID);
+    }
+}
